5e1ba03f54175ccdde1cf406009a5bb1c7cc19fe
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2025-12-31 17:48:51 +0300

    К шаблонам страниц добавлены стили Bootstrap

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,6 @@ app_name = CatalogConfig.name
 
 
 urlpatterns = [
-    path('home', home, name='home' ),
-    path('contacts', contacts, name='contacts' )
+    path('home/', home, name='home' ),
+    path('contacts/', contacts, name='contacts' )
 ]
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', RedirectView.as_view(pattern_name='catalog:home'), name='main_redirect'),
+
     path('catalog/', include('catalog.urls', namespace='catalog'))
 ]
