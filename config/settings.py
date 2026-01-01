fd21ca30eeea2e2aa7037bf7deb8cf033060025b
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-01-01 11:16:59 +0300

    Создана База данных django_project. Добавлены настройки для работы с БД, ключи убраны в файл .env

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!hn=5e8*#x$p*0ug0^49h5s+re047rsgg10e^17@zn7g@2mih*'
-
+SECRET_KEY=os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == "True" else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),    # Имя нашей базы
+        'USER': os.getenv('USER'),    # Имя пользователя PostgreSQL
+        'PASSWORD': os.getenv('PASSWORD'),    # Пароль пользователя PostgreSQL
+        'HOST': os.getenv('HOST'),          # Адрес сервера базы данных
+        'PORT': os.getenv('PORT'),          # Порт, на котором работает PostgreSQL
     }
 }
 
