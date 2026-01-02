04237bc86f29763fa2d5765fbc8a7eab6559f793
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-01-02 03:37:16 +0300

    Изменены миграции. Через shell добавлены Product и Category, выполнены фильтры

--- catalog/models.py
+++ catalog/models.py
@@ -1,20 +1,35 @@
 from django.db import models
+from django.conf import settings
 from django.core.validators import MinValueValidator
 
 # Create your models here.
 
+def get_default_category_id():
+    """Возвращает ID категории 'Без категории' или создает ее"""
+    # чтобы избежать циклического импорта
+    from django.apps import apps
+
+    category_model = apps.get_model('catalog', 'Category')
+    category, created = category_model.objects.get_or_create(
+        name="Без категории",
+        defaults={'descriptions': 'Товары без указанной категории'}
+    )
+    return category.id
+
 
 class Category(models.Model):
     name = models.CharField(
-        max_length=100,
+        max_length=150,
         verbose_name="Наименование категории",
         help_text="Введите название категории",
+        unique=True   # Названия Категории должны быть уникальными
     )
     descriptions = models.TextField(
         verbose_name="Описание",
         help_text="Введите описание категории",
         blank=True,
         null=True,
+        max_length=2000,
     )
 
     class Meta:
@@ -28,38 +43,43 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(
-        max_length=100,
+        max_length=150,
         verbose_name="Наименование товара",
         help_text="Введите название товара",
+        db_index=True,    # Создает индекс в БД для быстрого поиска и сортировки
     )
     descriptions = models.TextField(
         verbose_name="Описание",
         help_text="Введите описание товара",
         blank=True,
         null=True,
+        max_length=2000,
     )
     image = models.ImageField(
         upload_to="catalog/product_photo",
-        blank=True,
+        blank=settings.DEBUG,  # Разрешаем пустое ТОЛЬКО в режиме отладки (DEBUG=True)
         null=True,
         verbose_name="Фото",
         help_text="Загрузите фото товара",
     )  #
     category = models.ForeignKey(
         Category,
-        on_delete=models.SET_NULL,
+        on_delete=models.SET_DEFAULT,   # При удалении категории, у товара category станет DEFAULT
+        default=get_default_category_id,  # Используем функцию
         verbose_name="Категория",
         help_text="Введите категорию товара",
-        blank=True,
-        null=True,
-        related_name="products",
+        related_name="products",   # Позволяет получать все товары категории: category.products.all()
     )
     purchase_price = models.DecimalField(
         max_digits=10,  # До 10 млн рублей
         decimal_places=2,  # 2 знака = копейки
-        validators=[MinValueValidator(0)],  # Цена не может быть отрицательной
+        validators=[MinValueValidator(
+            1,  # Минимальная цена - 1 рубль
+            message="Цена должна быть больше нуля. Минимум 1 рубль"
+                                      )
+                    ],
         verbose_name="Цена за покупку (руб.)",
-        help_text="Укажите цену в рублях с копейками",
+        help_text="Укажите цену в рублях с копейками. Минимальная цена: 1 рубль",
     )
     created_at = models.DateTimeField(
         auto_now_add=True,  # Автоматически при создании
