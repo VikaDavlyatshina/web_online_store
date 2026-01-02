a6472cc31c99c9d98eb516ccc07bd2a04bb03068
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-01-02 15:13:11 +0300

    Доработан контроллер Главной страницы для вывода информации о товарах в консоль

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "catalog/home.html")
+    """ Контроллер Главной страницы """
+
+    # Выборка последних 5 товаров по дате создания
+    # order_by('-created_at') -> сортировка по убыванию даты (новые первыми)
+    # [:5] → берём первые
+    latest_products = Product.objects.order_by('-created_at')[:5]
+
+
+    # Вывод в консоль
+    print("Последние 5 товаров (вывод в консоль):")
+    for i, product in enumerate(latest_products, 1):
+        print(f"{i}. {product.name}\n"
+              f"Цена: {product.purchase_price}\n"
+              f"Категория: {product.category.name if product.category else 'Без категории'}\n"
+              f"Создан: {product.created_at}\n")
+
+    # Передаём в шаблон
+    context = {
+        'title': 'Главная страница',
+        'latest_product': latest_products
+    }
+
+    return render(request, "catalog/home.html", context)
 
 
 def contacts(request):
