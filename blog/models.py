5cae6fca1643ac69ec06f6a758f3cfde36a17275
vika <vika.davlyatshina@mail.ru>
2026-01-16 20:28:25 +0300

    Добавление картинок и постов

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
+from django.utils import timezone
+
+from django.core.mail import send_mail
 from django.db import models
 from django.urls import reverse
 from django.conf import settings
+
+
 
 # Create your models here.
 
@@ -42,6 +47,13 @@ class BlogPost(models.Model):
         default=0,  # Поле для хранения просмотров
     )
 
+    email_sent_at = models.DateTimeField(
+        verbose_name="Дата отправки email о 100 просмотрах",
+        null=True,
+        blank=True,
+        help_text="Когда было отправлено письмо о достижении 100 просмотров"
+    )
+
     class Meta:
         verbose_name = "Запись блога"
         verbose_name_plural = "Записи блога"
@@ -53,3 +65,41 @@ class BlogPost(models.Model):
     def get_absolute_url(self):
         """Получение абсолютной ссылки для Поста блога"""
         return reverse("blog:blogpost_detail", kwargs={"pk": self.pk})
+
+    # def save(self, *args, **kwargs):
+    #     """Переопределяем save для отправки email при 100 просмотрах"""
+    #
+    #     # Условия:
+    #     # 1. Просмотров больше 100
+    #     # 2. Email еще не отправлялся (email_sent_at пустое)
+    #     # 3. Это существующая запись (есть pk)
+    #
+    #     should_send_email = (
+    #         self.views_count >= 100 and
+    #         not self.email_sent_at
+    #         and self.pk is not None
+    #     )
+    #
+    #     super().save(*args, **kwargs)
+    #
+    #     if should_send_email:
+    #         self.send_100_views_email()
+    #         self.email_sent_at = timezone.now()
+    #         BlogPost.objects.filter(pk=self.pk).update(email_sent_at=timezone.now())
+    #
+    # def send_100_views_email(self):
+    #     """Функция для отправки письма при 100 просмотрах"""
+    #
+    #     subject = f"Ура! Статья {self.title} достигла 100 просмотров!"
+    #     message = (f"Дорогой администратор! Позравляем, ваша статья {self.title} достигла 100 просмотр"
+    #                f" Посмотреть статью: {self.get_absolute_url()}\n Поздравляем с популярностью")
+    #
+    #     send_mail(
+    #         subject=subject,
+    #         message=message,
+    #         from_email=settings.DEFAULT_FROM_EMAIL,
+    #         recipient_list=[settings.ADMIN_EMAIL],
+    #         fail_silently=False,
+    #     )
+
+
--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
+
+
 ALLOWED_HOSTS = ["*"]
 
 
