11ecaefea946d864f6ebe3a941da92fce5c8a58f
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-02-07 00:05:46 +0300

    Созданы представления для создания Пользователя и для редиректа при выходе из аккаунта.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from users.forms import UserRegisterForm
+
 
 # Create your views here.
+
+
+class UserCreateView(CreateView):
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
