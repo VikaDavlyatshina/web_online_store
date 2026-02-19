13dc8d1f940b85a15c0b50b75909e47ff6b0e5de
Davlystshina Victoria <vika.davlyatshina@mail.ru>
2026-02-19 17:12:28 +0300

    Добавление библиотеки redis для кэширования и его настроек в settings.py

--- config/settings.py
+++ config/settings.py
@@ -151,3 +151,11 @@ EMAIL_USE_SSL = True
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://localhost:6379",
+    }
+}
